0b4f245f9f75a7bd022b3d9954ee5b3f949d2077
Karl Kwon <newmkka@gmail.com>
2019-03-14 22:10:15 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactFlagsSelect from 'react-flags-select';
+import 'react-flags-select/css/react-flags-select.css';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+class Country extends React.Component {
+    render() {
+        return (
+            <div className="demo-source">
+                <ReactFlagsSelect
+                    searchable={true}
+                    searchPlaceholder="Search for a country" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Country />,
+    document.getElementById('country')
+);
+
+
+class Names extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('Names were submitted: \n' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    <textarea value={this.state.value} onChange={this.handleChange} placeholder="Please type a name" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Names />,
+    document.getElementById('name')
+);
